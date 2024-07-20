dcdf59bb4fd3d8dc526fc9673113243d9994e6f9
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-20 13:17:52 -0300

    adicionando exemplo 0 com valor fixo 10 no parametro valor_2

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,17 +1,24 @@
 
-def soma(valor_1: float, valor_2: float) -> float:
-    """
-    uma funcao simples de soma do tipo float que retorna um float
-    """
-    resultado_soma = valor_1 + valor_2
-    return resultado_soma
+# def soma(valor_1: float, valor_2: float) -> float:
+#     """
+#     uma funcao simples de soma do tipo float que retorna um float
+#     """
+#     resultado_soma = valor_1 + valor_2
+#     return resultado_soma
 
 
-valor_soma = soma(valor_1 = 4,valor_2 = 6)
+# valor_soma = soma(valor_1 = 4,valor_2 = 6)
 
-print(f'Resultado do primeiro exemplo de função ficou em: {valor_soma}')
+# print(f'Resultado do primeiro exemplo de função ficou em: {valor_soma}')
 
-valor_soma2 = soma(2.15,2.15)
-print(f'Resultado do segundo exemplo de função ficou em: {valor_soma2}')
+# valor_soma2 = soma(2.15,2.15)
+# print(f'Resultado do segundo exemplo de função ficou em: {valor_soma2}')
 
 
+
+def somar_10(valor_1: float, valor_2: float = 10) -> float:
+    somar_10 = valor_1 + valor_2
+    return somar_10
+
+resultado_soma = somar_10(10)
+print(resultado_soma)
