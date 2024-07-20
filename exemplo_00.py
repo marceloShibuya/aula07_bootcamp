aa12b8e858df9fba0a7535d77db41005f9a2947d
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-20 13:08:13 -0300

    adicionando exemplo 0 de funcao

---
+++ exemplo_00.py
@@ -0,0 +1,17 @@
+
+def soma(valor_1: float, valor_2: float) -> float:
+    """
+    uma funcao simples de soma do tipo float que retorna um float
+    """
+    resultado_soma = valor_1 + valor_2
+    return resultado_soma
+
+
+valor_soma = soma(valor_1 = 4,valor_2 = 6)
+
+print(f'Resultado do primeiro exemplo de função ficou em: {valor_soma}')
+
+valor_soma2 = soma(2.15,2.15)
+print(f'Resultado do segundo exemplo de função ficou em: {valor_soma2}')
+
+
